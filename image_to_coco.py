6439db5c90def3e08fc21746daf37b4f8cc978cd
Fam Awad <fam@awadlouis.com>
2024-06-13 15:42:44 +0200

    Added keypoints to annotations

--- image_to_coco.py
+++ image_to_coco.py
@@ -44,7 +44,7 @@ ORIGINAL_EXTENTION = 'png'
 image_id = 0
 annotation_id = 0
 
-def images_annotations_info(keypoints_path: str, bboxpath: str, start_end_points: bool=True) -> tuple[int, int, int]:
+def images_annotations_info(keypoints_path: str, bbox_path: str, start_end_points: bool=True) -> tuple[int, int, int]:
     '''
         Process the image data and generate annotations information.
 
@@ -61,22 +61,22 @@ def images_annotations_info(keypoints_path: str, bboxpath: str, start_end_points
     images = []
 
     # Iterate through categories and corresponding masks
-    for bbox_file, keypoints_file in zip(glob.glob(os.path.join(bboxpath, f'*.{BBOX_EXTENTION}')), glob.glob(os.path.join(keypoints_file, f'*.{KEYPOINTS_EXTENTION}'))):
+    for bbox_file, keypoints_file in zip(glob.glob(os.path.join(bbox_path, f'*.{BBOX_EXTENTION}')), glob.glob(os.path.join(keypoints_path, f'*.{KEYPOINTS_EXTENTION}'))):
         
-        bbox = [] 
-        keypoints = []
+        bboxes = []
+        total_keypoints = []
         
         # Loading bbox
         with open(bbox_file, 'r') as file:
             
-            lines = file.readlines()
-        
+            lines = file.readlines()    
+                    
             # Create annotation for each box
             for line in lines:
 
                 # Processing the line 
                 try: 
-                    keypoints_coordinates = list(map(int, line.split()))
+                    bbox_coordinates = list(map(int, line.split()))
                 except ValueError: 
                     
                     # ValueError: If the coordinates are not integers
@@ -86,15 +86,16 @@ def images_annotations_info(keypoints_path: str, bboxpath: str, start_end_points
                 if start_end_points:
                     
                     # Calulate the width and height from the bbox coordinates
-                    x = keypoints_coordinates[0]
-                    y = keypoints_coordinates[1]
-                    w = keypoints_coordinates[2] - keypoints_coordinates[0]
-                    h = keypoints_coordinates[3] - keypoints_coordinates[1]
+                    x = min(bbox_coordinates[0], bbox_coordinates[2])
+                    y = min(bbox_coordinates[1], bbox_coordinates[3])
+                    w = abs(bbox_coordinates[2] - bbox_coordinates[0])
+                    h = abs(bbox_coordinates[3] - bbox_coordinates[1])
                     
                     bbox = (x, y, w, h)
                 else: 
-                    bbox = (tuple(keypoints_coordinates))
+                    bbox = (tuple(bbox_coordinates))
                 
+                bboxes.append(bbox)
 
     
         # Loading keypoints
@@ -116,38 +117,42 @@ def images_annotations_info(keypoints_path: str, bboxpath: str, start_end_points
                 if start_end_points:
                     
                     # Calulate the width and height from the bbox coordinates
-                    x = keypoints_coordinates[0]
-                    y = keypoints_coordinates[1]
-                    w = keypoints_coordinates[2] - keypoints_coordinates[0]
-                    h = keypoints_coordinates[3] - keypoints_coordinates[1]
+                    x = min(keypoints_coordinates[0], keypoints_coordinates[2])
+                    y = min(keypoints_coordinates[1], keypoints_coordinates[3])
+                    w = abs(keypoints_coordinates[2] - keypoints_coordinates[0])
+                    h = abs(keypoints_coordinates[3] - keypoints_coordinates[1])
                     
                     keypoints = (x, y, w, h)
                 else: 
                     keypoints = (tuple(keypoints_coordinates))
                     
-                    
+                total_keypoints.append(keypoints)
+        
         image_id = bbox_file.split('/')[-1].split('.')[0]
+        
+        for bbox, keypoints in zip(bboxes, total_keypoints):
+                    
+            annotation = {
+                "iscrowd": 0,
+                "id": annotation_id,
+                "image_id": image_id,
+                "category_id": category_ids['Ant'],
+                "bbox": bbox,
+                "keypoints": keypoints,
+            }
 
-        annotation = {
-            "iscrowd": 0,
-            "id": annotation_id,
-            "image_id": image_id,
-            "category_id": category_ids['Ant'],
-            "bbox": bbox,
-            "keypoints": keypoints,
-        }
-
-        annotations.append(annotation)
-        annotation_id += 1
+            annotations.append(annotation)
+            annotation_id += 1
 
     return images, annotations, annotation_id
 
 
-def process_masks(mask_path, dest_json):
+def process_masks(bbox_path: str, keypoints_path: str, dest_json: str) -> None:
     global image_id, annotation_id
     image_id = 0
     annotation_id = 0
 
+    # TODO: Add rest of coco information
     # Initialize the COCO JSON format with categories
     coco_format = {
         "info": {},
@@ -158,19 +163,21 @@ def process_masks(mask_path, dest_json):
     }
 
     # Create images and annotations sections
-    coco_format["images"], coco_format["annotations"], annotation_cnt = images_annotations_info(mask_path)
+    coco_format["images"], coco_format["annotations"], annotation_cnt = images_annotations_info(keypoints_path, bbox_path)
 
     # Save the COCO JSON to a file
     with open(dest_json, "w") as outfile:
         json.dump(coco_format, outfile, sort_keys=True, indent=4)
 
-    print("Created %d annotations for images in folder: %s" % (annotation_cnt, mask_path))
+    print("Created %d annotations for images in folder: %s" % (annotation_cnt, bbox_path))
 
 if __name__ == "__main__":
-    train_bbox_path = "datasets\\dataset1\\data\\Train_data\\bboxes\\"
-    train_json_path = "datasets\\dataset1\\data\\Train_data\\test.json"
-    process_masks(train_bbox_path, train_json_path)
+    train_bbox_path = "data\\Train_data\\bboxes\\"
+    train_json_path = "data\\Train_data\\train.json"
+    train_keypoints_path = "data\\Train_data\\keypoints\\"
+    process_masks(train_bbox_path, train_keypoints_path, train_json_path)
 
-    test_mask_path = "datasets\\dataset1\\data\\Test_data\\bboxes\\"
-    test_json_path = "datasets\\dataset1\\data\\Test_data\\test.json"
-    process_masks(test_mask_path, test_json_path)
+    test_mask_path = "data\\Test_data\\bboxes\\"
+    test_json_path = "data\\Test_data\\test.json"
+    test_keypoints_path = "data\\Test_data\\keypoints\\"
+    process_masks(test_mask_path, test_keypoints_path, test_json_path)
