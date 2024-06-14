ee8d5155d389251efc67ea4661636581545540d1
Fam Awad <fam@awadlouis.com>
2024-06-14 15:01:11 +0200

    Added docstrings to functions and classes

--- models/yolov8/yolov8.py
+++ models/yolov8/yolov8.py
@@ -2,7 +2,15 @@ from ultralytics import YOLO
 import yaml
 
 class YOLOv8AntDetector:
-    
+    '''
+        YOLOv8AntDetector class is a wrapper for the YOLOv8 model. It is used to train the model, 
+        predict the bounding boxes of ants in images, and evaluate the model.
+        
+        Parameters:
+            - data_path (str): The path to the data file that contains the number of classes. (Only in development mode)
+            - best_model_path (str): The path to the best model file. (Only in production mode)
+            - dev (bool): Development mode. If True, the model is in development mode and can be trained. Default is True.
+    '''
     def __init__(self, data_path:str=None, best_model_path:str=None , dev:bool=True) -> None: 
         
         # Development mode
@@ -37,7 +45,16 @@ class YOLOv8AntDetector:
             self.dev = False
             
     def train(self, project_result_path:str, patience:int=0, epochs:int=200, batch_size:int=16, img_size:int=640) -> None:
-        
+        '''
+            Trains the model on the given dataset.
+            
+            Parameters:
+                - project_result_path (str): The path to the directory where the results of the training will be saved.
+                - patience (int): The number of epochs with no improvement after which training will be stopped. Default is 0.
+                - epochs (int): The number of epochs to train the model. Default is 200.
+                - batch_size (int): The batch size used in training. Default is 16.
+                - img_size (int): The size of the input image. Default is 640.
+        '''
         if not self.dev: 
             raise Exception("Training is only available in development mode.")
     
@@ -49,10 +66,30 @@ class YOLOv8AntDetector:
                                     patience=patience,
                                     imgsz=img_size)
         
-    def predict(self, img_path:str, confidence:float=0.2) -> dict:
+    def predict(self, img_path:str, confidence:float=0.2) -> list:
+        '''
+            Predicts the bounding boxes of ants in the given image.
+            
+            Parameters:
+                - img_path (str): The path to the image.
+                - confidence (float): The confidence threshold. Default is 0.2.
+                
+            Returns:
+                - list: A list containing the resulting images from the prediction.
+        '''
         return self.model.predict(img_path, conf=confidence)
     
-    def predict(self, img_paths:list[str], confidence:float=0.2) -> dict:
+    def predict(self, img_paths:list[str], confidence:float=0.2) -> list:
+        '''
+            Predicts the bounding boxes of ants in the given array of images.
+            
+            Parameters:
+                - img_paths (list[str]): A list of paths to the images.
+                - confidence (float): The confidence threshold. Default is 0.2.
+                
+            Returns:
+                - dict: A list containing the resulting images from the prediction.
+        '''
         return self.model.predict(img_paths, conf=confidence)
 
 if __name__ == "__main__":
